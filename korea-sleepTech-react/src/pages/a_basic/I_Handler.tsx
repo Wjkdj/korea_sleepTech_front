8e03e1012009a7353c0ac3262a879f61376dc2a0
Wjkdj <wlsdnxo0807@gmail.com>
2025-04-01 09:20:07 +0900

    20250401 JWT

---
+++ korea-sleepTech-react/src/pages/a_basic/I_Handler.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function I_Handler() {
+  return (
+    <div>I_Handler</div>
+  )
+}
+
+export default I_Handler
---
+++ korea-sleepTech-react/src/pages/a_basic/index.tsx
@@ -0,0 +1,23 @@
+// index.tsx
+import React from "react";
+import B_React_Counter from "../B_React_Counter";
+
+function Index() {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+        }}
+      >
+        리액트 기본 문법
+      </h1>
+
+      <h2>리액트 VS 타입스크립트(카운터 예제)</h2>
+      <B_React_Counter />
+    </div>
+  );
+}
+
+export default Index; // 기본 내보내기 - 사용하면서 컴포넌트명 변경 가능
