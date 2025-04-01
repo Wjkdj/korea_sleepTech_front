c9cf92e512e20e52f20a7f97b864cb06a08f3f32
Wjkdj <wlsdnxo0807@gmail.com>
2025-04-01 15:15:31 +0900

    20250401 JWT

---
+++ korea-sleepTech-react/src/pages/b_hooks/a_useState/UseState03.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// ! 리액트 이벤트 객체 타입 정의
+
+// == 이벤트 타입 == 
+// : 이벤트 종류에 따라 타입 정의가 상이
+// EX) 입력 필드에 대한 변경 이벤트: React.ChangeEvent
+// EX) 클릭에 대한 변경 이벤트: React.ClickEvent
+// EX) 폼 제출에 대한 변경 이벤트: React.formEvent
+//    >> 폼 제출 기본 동작 방지 e.preventDefault();
+
+function UseState03() {
+  return (
+    <div>UseState03</div>
+  )
+}
+
+export default UseState03
--- korea-sleepTech-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import UseState01 from './UseState01';
-import UseState02 from './UseState02';
+import React from "react";
+import UseState01 from "./a_useState/UseState01";
+import UseState02 from "./a_useState/UseState02";
+import UseState03 from "./a_useState/UseState03";
+import UseState04 from "./a_useState/UseState04";
+import UseState05 from "./a_useState/UseState05";
+import UseState06 from "./a_useState/UseState06";
 
 const h2Style = {
   backgroundColor: "black",
@@ -11,19 +15,23 @@ function Index() {
   return (
     <div>
       <h1
-          style={{
-            backgroundColor: "black",
-            color: "pink",
-          }}
-        >
-          === 리액트 Hooks ===
-        </h1>
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+        }}
+      >
+        === 리액트 Hooks ===
+      </h1>
 
-        <h2 style={h2Style}>리액트 Hooks - useState</h2>
-        <UseState01 /> <hr />
-        <UseState02 />
+      <h2 style={h2Style}>리액트 Hooks - useState</h2>
+      <UseState01 /> <hr />
+      <UseState02 /> <hr />
+      <UseState03 /> <hr />
+      <UseState04 /> <hr />
+      <UseState05 /> <hr />
+      <UseState06 /> <hr />
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;
